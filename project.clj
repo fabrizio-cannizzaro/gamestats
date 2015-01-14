ac1df08eb048186a51fc6efa0971eee2729a00ca
fabrix <alcinoo.cannizzaro@gmail.com>
2015-01-14 08:51:54 +0100

    format

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;Copyright (C) 2015, Fabrizio Cannizzaro alcinoo.cannizzaro@gmail.com
-;This file is part of gamestats.
+; Copyright (C) 2015, Fabrizio Cannizzaro - alcinoo.cannizzaro@gmail.com
+; This file is part of gamestats.
 ;
 ;    gamestats is free software: you can redistribute it and/or modify
 ;    it under the terms of the GNU General Public License as published by
